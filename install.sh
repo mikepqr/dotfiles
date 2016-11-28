52eff6b82995987604194526735d8867a6b709c9
Mike Williams <mike@mike.place>
2016-11-27 19:12:17 -0500

    Add README and run install.sh from anywhere

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
+cd "$(dirname "${BASH_SOURCE[0]}")"  || exit
 stow $(echo */)
-
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 vim -c PlugInstall +qall
