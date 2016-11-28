c90554e4b0371707f2ef6f08abaa5431f65c2107
Mike Williams <mike@mike.place>
2016-11-27 21:59:33 -0500

    Install without stow

--- install.sh
+++ install.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cd "$(dirname "${BASH_SOURCE[0]}")"  || exit
-stow $(echo */)
+set -e
+set -u
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if command -v stow >/dev/null 2>&1; then
+    cd "$BASEDIR" || exit
+    stow "$(echo */)"
+else
+    find "$BASEDIR"/*/ -maxdepth 0 -exec cp -sfRT {} "${HOME}" \;
+fi
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall +qall
