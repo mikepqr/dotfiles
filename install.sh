2d1583e80c184814009e767748389acaa16312a5
Mike Lee Williams <mike@mike.place>
2017-01-07 17:22:50 -0500

    Use find rather than glob to list packages

--- install.sh
+++ install.sh
@@ -2,16 +2,20 @@
 
 set -e
 set -u
-mkdir "$HOME/bin"
+mkdir -p "$HOME/bin"
 
-BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+basedir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+dirs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
 
-if command -v stow >/dev/null 2>&1; then
-    cd "$BASEDIR" || exit
-    stow "$(echo */)"
-else
-    find "$BASEDIR"/*/ -maxdepth 0 -exec cp -sfRT {} "${HOME}" \;
-fi
+for d in $dirs
+do
+    echo "Installing $d configuration"
+    if command -v stow >/dev/null 2>&1; then
+        stow "$d"
+    else
+        find "$basedir/$d/" -maxdepth 0 -exec cp -sfRT {} "${HOME}" \;
+    fi
+done
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall +qall
