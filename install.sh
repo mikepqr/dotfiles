f09c1ac7bd1bf45ee2b646bcb1861ca57724a63a
Mike Lee Williams <mike@mike.place>
2017-01-23 02:09:02 -0500

    curl directly to destination

--- install.sh
+++ install.sh
@@ -3,14 +3,13 @@
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
 shtowurl=https://raw.githubusercontent.com/williamsmj/shtow/master/shtow
-curl -O $shtowurl
-chmod +x shtow
-mv shtow bin/bin
+curl -o bin/bin/shtow $shtowurl
+chmod +x bin/bin/shtow
 
 for pkg in $pkgs
 do
     echo "Installing $pkg configuration"
-    bash bin/bin/shtow "$pkg"
+    bin/bin/shtow "$pkg"
 done
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
