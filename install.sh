cd2c995505db68e4d85d5319ec10275f564fa282
Mike Lee Williams <mike@mike.place>
2017-01-23 01:44:44 -0500

    Hack to add shtow to bin/bin

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
 pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
-stowurl=https://raw.githubusercontent.com/williamsmj/shtow/master/shtow
-curl -O $stowurl
+shtowurl=https://raw.githubusercontent.com/williamsmj/shtow/master/shtow
+curl -O $shtowurl
 chmod +x shtow
+mv shtow bin/bin
 
 for pkg in $pkgs
 do
     echo "Installing $pkg configuration"
-    bash ./shtow "$pkg"
+    bash bin/bin/shtow "$pkg"
 done
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall +qall
-rm shtow
