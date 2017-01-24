288236344d5104288ca16448224ef485305e416f
Mike Lee Williams <mike@mike.place>
2017-01-23 22:04:56 -0500

    s/shtow/stowsh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
-shtowurl=https://raw.githubusercontent.com/williamsmj/shtow/master/shtow
-curl -o bin/bin/shtow $shtowurl
-chmod +x bin/bin/shtow
+stowshurl=https://raw.githubusercontent.com/williamsmj/stowsh/master/stowsh
+curl -o bin/bin/stowsh $stowshurl
+chmod +x bin/bin/stowsh
 
 pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
 pkgs=${pkgs//darwin/}
@@ -12,15 +12,15 @@ pkgs=${pkgs//linux/}
 for pkg in $pkgs
 do
     echo "Installing $pkg configuration"
-    bin/bin/shtow "$pkg"
+    bin/bin/stowsh "$pkg"
 done
 
 if [[ $(uname) == "Darwin" ]] ; then
-    bin/bin/shtow darwin
+    bin/bin/stowsh darwin
 fi
 
 if [[ $(uname) == "Linux" ]] ; then
-    bin/bin/shtow linux
+    bin/bin/stowsh linux
 fi
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
