084ecdad17660046b761a18c3018b40cea30a5f1
Mike Lee Williams <mike@mike.place>
2017-01-25 11:50:10 -0500

    Add comments to install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-
 cd "$(dirname "${BASH_SOURCE[0]}")" || exit
-stowshurl=https://raw.githubusercontent.com/williamsmj/stowsh/master/stowsh
-curl -o bin/bin/stowsh $stowshurl
+
+# get stowsh (and put it in a package)
+curl -o bin/bin/stowsh https://raw.githubusercontent.com/williamsmj/stowsh/master/stowsh
 chmod +x bin/bin/stowsh
 
+# get all the root level directories that don't start with "."
 pkgs="$(find . -maxdepth 1 ! -name '.*' -type d | sed "s|./||")"
+# remove OS-specific packages from this list
 pkgs=${pkgs//darwin/}
 pkgs=${pkgs//linux/}
 
 for pkg in $pkgs
 do
     echo "Installing $pkg configuration"
-    bin/bin/stowsh "$pkg"
+    bin/bin/stowsh -s "$pkg"
 done
 
+# install OS-specific packages if appropriate
 if [[ $(uname) == "Darwin" ]] ; then
     bin/bin/stowsh darwin
 fi
@@ -23,5 +26,6 @@ if [[ $(uname) == "Linux" ]] ; then
     bin/bin/stowsh linux
 fi
 
+# bootstrap vim plugin configuration
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall +qall
