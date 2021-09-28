da4e5598cf1e47867c56e7b015c45ceb7babb2dc
Mike Lee Williams <mike@mike.place>
2021-09-27 21:41:27 -0700

    Update LSP configuration
     - move bindings into on_attach so they only clobber defaults in buffers
       with attached LSP
     - enable nerdfont gutter icons
     - cut some stuff I didn't understand related to documentHighlight
     - customize the lua LSP config so that the unknown vim global is not
       treated as an error

--- vim/.config/nvim/lua/lsp.lua
+++ vim/.config/nvim/lua/lsp.lua
@@ -12,15 +12,15 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   }
 )
 
--- -- uncomment below to enable nerd-font based icons for diagnostics in the
--- -- gutter, see:
--- -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
--- local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
---
--- for type, icon in pairs(signs) do
---   local hl = "LspDiagnosticsSign" .. type
---   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
--- end
+ -- uncomment below to enable nerd-font based icons for diagnostics in the
+ -- gutter, see:
+ -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
+ local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
+
+ for type, icon in pairs(signs) do
+   local hl = "LspDiagnosticsSign" .. type
+   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+ end
 
 -- symbols for autocomplete
 vim.lsp.protocol.CompletionItemKind = {
@@ -51,41 +51,42 @@ vim.lsp.protocol.CompletionItemKind = {
     "   (TypeParameter)"
 }
 
-local function documentHighlight(client, bufnr)
-    -- Set autocommands conditional on server_capabilities
-    if client.resolved_capabilities.document_highlight then
-        vim.api.nvim_exec(
-            [[
-      hi LspReferenceRead cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceText cterm=bold ctermbg=red guibg=#464646
-      hi LspReferenceWrite cterm=bold ctermbg=red guibg=#464646
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-            false
-        )
-    end
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+  local opts = { noremap=true, silent=true }
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  buf_set_keymap('n', 'K', '<cmd>Lspsaga hover_doc<CR>', opts)
+  buf_set_keymap('n', '<C-K>', '<cmd>Lspsaga signature_help<CR>', opts)
+  buf_set_keymap('n', 'gl', '<cmd>Lspsaga show_line_diagnostics<CR>', opts)
+  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
+  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
+  buf_set_keymap('n', 'gF', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local lsp_config = {}
-
-function lsp_config.common_on_attach(client, bufnr)
-    documentHighlight(client, bufnr)
-end
-function lsp_config.tsserver_on_attach(client, bufnr)
-    lsp_config.common_on_attach(client, bufnr)
-    client.resolved_capabilities.document_formatting = false
-end
 
 local function setup_servers()
   require'lspinstall'.setup()
   local servers = require'lspinstall'.installed_servers()
   for _, server in pairs(servers) do
-    require'lspconfig'[server].setup{}
+    require'lspconfig'[server].setup{
+        on_attach = on_attach
+    }
   end
+  -- overwrite lua config
+  require'lspconfig'.lua.setup {
+    on_attach = on_attach,
+    settings = {
+      Lua = {
+        diagnostics = {
+          -- recognize the `vim` global
+          globals = {'vim'},
+        },
+      },
+    },
+  }
 end
 
 setup_servers()
