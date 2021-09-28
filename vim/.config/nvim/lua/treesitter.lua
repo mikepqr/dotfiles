bd5b3bad039c18b638eca3381ce0e29a7122ded3
Mike Lee Williams <mike@mike.place>
2021-09-27 21:58:20 -0700

    Copy defaults.nvim treesitter configuration
    https://github.com/mjlbach/defaults.nvim

--- vim/.config/nvim/lua/treesitter.lua
+++ vim/.config/nvim/lua/treesitter.lua
@@ -1,34 +1,50 @@
-require'nvim-treesitter.configs'.setup {
-    -- ensure_installed can be "all" or a list of languages { "python", "javascript" }
-    ensure_installed = {"python", "bash", "javascript", "go", "vim"},
-
-    highlight = { -- enable highlighting for all file types
-      enable = true, -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
+require('nvim-treesitter.configs').setup {
+  highlight = {
+    enable = true, -- false will disable the whole extension
+  },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
     },
-    incremental_selection = {
-      enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
-      disable = { "cpp", "lua" },
-      keymaps = {                       -- mappings for incremental selection (visual mappings)
-        init_selection = "gnn",         -- maps in normal mode to init the node/scope selection
-        node_incremental = "grn",       -- increment to the upper named parent
-        scope_incremental = "grc",      -- increment to the upper scope (as defined in locals.scm)
-        node_decremental = "grm",       -- decrement to the previous node
-      }
-    },
-    textobjects = {
-      -- These are provided by
-      select = {
-        enable = true,  -- you can also use a table with list of langs here (e.g. { "python", "javascript" })
-        keymaps = {
-          -- You can use the capture groups defined here:
-	  -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects/blob/master/queries/c/textobjects.scm
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ab"] = "@block.outer",
-          ["ib"] = "@block.inner",
-          ["as"] = "@statement.outer",
-          ["is"] = "@statement.inner",
-        },
+  },
+  indent = {
+    enable = true,
+  },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      keymaps = {
+        -- You can use the capture groups defined in textobjects.scm
+        ['af'] = '@function.outer',
+        ['if'] = '@function.inner',
+        ['ac'] = '@class.outer',
+        ['ic'] = '@class.inner',
       },
     },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
+      },
+    },
+  },
 }
