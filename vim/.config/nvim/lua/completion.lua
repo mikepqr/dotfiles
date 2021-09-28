139e264bc722ac1caa7f79045ef9cac5814d6675
Mike Lee Williams <mike@mike.place>
2021-09-27 22:18:27 -0700

    Disable autocompletion and use tab
    Based on
    https://github.com/hrsh7th/nvim-cmp/wiki/Example-mappings#super-tab-like-mapping
    and https://github.com/mjlbach/defaults.nvim.

--- vim/.config/nvim/lua/completion.lua
+++ vim/.config/nvim/lua/completion.lua
@@ -1,7 +1,15 @@
 vim.o.completeopt = "menuone,noselect"
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+end
+
 local cmp = require'cmp'
 cmp.setup {
+  completion = {
+    autocomplete = false
+  },
   mapping = {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
@@ -9,6 +17,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
+    ['<esc>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
@@ -16,6 +25,8 @@ cmp.setup {
     ['<Tab>'] = function(fallback)
       if vim.fn.pumvisible() == 1 then
         vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
