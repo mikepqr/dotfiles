bf37f53844b1a762d004d095463cbd75f5dedf0e
Mike Lee Williams <mike@mike.place>
2021-11-04 00:02:39 -0700

    Rework completion configuration based on https://www.youtube.com/watch?v=_DnmphIwnjo

--- vim/.config/nvim/lua/completion.lua
+++ vim/.config/nvim/lua/completion.lua
@@ -1,45 +1,41 @@
-local has_words_before = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
-end
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
 local cmp = require'cmp'
 cmp.setup {
   completion = {
     autocomplete = false,
-    completeopt = 'menu,menuone,noinsert'
+    completeopt = 'menu,menuone,noinsert',
   },
   mapping = {
     ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<esc>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-n>', true, true, true), 'n')
-      elseif has_words_before() then
+    ['<esc>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm(
+      {
+        behavior = cmp.ConfirmBehavior.Insert,
+        select = true,
+      },
+      {"i", "c"}
+    ),
+    ["<Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
         cmp.complete()
-      else
-        fallback()
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if vim.fn.pumvisible() == 1 then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<C-p>', true, true, true), 'n')
-      else
-        fallback()
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
       end
-    end,
+    end, { "i", "s" }),
   },
   sources = {
     { name = 'nvim_lsp' },
     { name = 'buffer' },
+    { name = 'path' },
   },
+  experimental = {
+    -- native_menu = false,
+  }
 }
