4b2bad014eec897f4a18a7f86ed0f258baeeb6f4
Mike Lee Williams <mike@mike.place>
2022-04-01 23:31:33 -0700

    Modify treesitter configuration

--- vim/.config/nvim/lua/treesitter.lua
+++ vim/.config/nvim/lua/treesitter.lua
@@ -4,7 +4,8 @@ require('nvim-treesitter.configs').setup {
     "python"
   },
   highlight = {
-    enable = true, -- false will disable the whole extension
+    enable = true,
+    -- disable = {"python"}, -- https://github.com/nvim-treesitter/nvim-treesitter/issues/1573#issuecomment-1068634000
   },
   incremental_selection = {
     enable = true,
@@ -24,32 +25,34 @@ require('nvim-treesitter.configs').setup {
       enable = true,
       lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
       keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
+        -- https://github.com/nvim-treesitter/nvim-treesitter-textobjects#built-in-textobjects
+        ['ab'] = '@block.outer',
+        ['ib'] = '@block.inner',
         ['af'] = '@function.outer',
         ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
+        -- ['ac'] = '@class.outer',
+        -- ['ic'] = '@class.inner',
       },
     },
     move = {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
+        [']]'] = '@function.outer',
+        [']m'] = '@class.outer',
       },
       goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
+        ['[['] = '@function.outer',
+        ['[m'] = '@class.outer',
       },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
+      -- goto_next_end = {
+      --   [']M'] = '@function.outer',
+      --   [']['] = '@class.outer',
+      -- },
+      -- goto_previous_end = {
+      --   ['[M'] = '@function.outer',
+      --   ['[]'] = '@class.outer',
+      -- },
     },
   },
 }
