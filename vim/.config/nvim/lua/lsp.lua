3dfa33b7b0d68a145bff606cb750e9a43ec9c635
Mike Lee Williams <mike@mike.place>
2022-04-02 23:12:35 -0700

    fix: update signcolumn symbols for new API
    At some point s/LspDiagnosticsSign/DiagnosticSign

--- vim/.config/nvim/lua/lsp.lua
+++ vim/.config/nvim/lua/lsp.lua
@@ -1,10 +1,9 @@
 -- Enable nerd-font based icons for diagnostics in the gutter, see:
 -- https://github.com/neovim/nvim-lspconfig/wiki/UI-customization#change-diagnostic-symbols-in-the-sign-column-gutter
-local signs = { Error = " ", Warning = " ", Hint = " ", Information = " " }
-
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "LspDiagnosticsSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 -- Use an on_attach function to only map the following keys
