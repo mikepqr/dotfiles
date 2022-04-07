fcf9fe84fae5e19eb6ac691742c14f3076522fe0
Mike Lee Williams <mike@mike.place>
2022-04-06 17:55:00 -0700

    Remove luasnip from completion settings (don't use it)

--- vim/.config/nvim/lua/completion.lua
+++ vim/.config/nvim/lua/completion.lua
@@ -17,8 +17,6 @@ cmp.setup {
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -26,8 +24,6 @@ cmp.setup {
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
