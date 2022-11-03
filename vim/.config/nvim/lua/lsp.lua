af67f8452f3883876af5a5667b1809bca1c7a2ed
Mike Lee Williams <mike@mike.place>
2022-11-03 15:43:32 -0700

    Update deprecated method call

--- vim/.config/nvim/lua/lsp.lua
+++ vim/.config/nvim/lua/lsp.lua
@@ -23,7 +23,7 @@ end
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 -- Configure lua language server for neovim development
 local lua_settings = {
