7ab52db1bcbb3c9a35a4e32c74d79111272ab1e1
Mike Lee Williams <mike@mike.place>
2022-11-03 22:46:29 -0700

    Disable custom black line length in null-ls

--- vim/.config/nvim/lua/nullls.lua
+++ vim/.config/nvim/lua/nullls.lua
@@ -1,9 +1,7 @@
 local null_ls = require("null-ls")
 
 local sources = {
-  null_ls.builtins.formatting.black.with({
-    extra_args = { "--line-length=100" },
-  }),
+  null_ls.builtins.formatting.black,
   null_ls.builtins.formatting.isort,
   null_ls.builtins.diagnostics.flake8.with({
     extra_args = { "--append-config", vim.fn.expand("~/.config/flake8") }
