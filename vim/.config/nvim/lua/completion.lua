2ac9b024161b7781820706b615ab7a47e61d36ea
Mike Lee Williams <mike@mike.place>
2023-01-29 23:13:50 -0800

    Use tab to invoke completion

--- vim/.config/nvim/lua/completion.lua
+++ vim/.config/nvim/lua/completion.lua
@@ -1,5 +1,11 @@
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require'cmp'
 local luasnip = require 'luasnip'
 cmp.setup {
@@ -16,6 +22,8 @@ cmp.setup {
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
