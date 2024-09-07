638415560ef3c644d4ba9eecd544bb03df0859f7
Mike Lee Williams <mike@mike.place>
2024-09-06 22:35:29 -0400

    Switch to none-ls

--- vim/.config/nvim/lua/nullls.lua
+++ vim/.config/nvim/lua/nullls.lua
@@ -3,7 +3,7 @@ local null_ls = require("null-ls")
 local sources = {
   null_ls.builtins.formatting.black,
   null_ls.builtins.formatting.isort,
-  null_ls.builtins.diagnostics.flake8.with({
+  require("none-ls.diagnostics.flake8").with({
     extra_args = { "--append-config", vim.fn.expand("~/.config/flake8") }
   }),
   null_ls.builtins.formatting.shfmt.with({
