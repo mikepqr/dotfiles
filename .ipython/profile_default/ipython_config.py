46c4337fb75fdb559da19e33bb6fe94f8f736321
Mike Lee Williams <mike@mike.place>
2025-12-20 22:05:34 -0500

    Clean up obsolete python config
    - Remove pythonrc.py: Python 3.13 has built-in tab completion
    - Remove pip.conf: uv handles virtualenvs automatically
    - Consolidate ipython autoreload into ipython_config.py

--- .ipython/profile_default/ipython_config.py
+++ .ipython/profile_default/ipython_config.py
@@ -1,2 +1,3 @@
-# Shortcut style to use at the prompt. 'vi' or 'emacs'.
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.InteractiveShellApp.extensions = ['autoreload']
+c.InteractiveShellApp.exec_lines = ['%autoreload 2']
